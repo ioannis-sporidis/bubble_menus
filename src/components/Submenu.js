fe839454053336e9e34d85fb23f77c63ad1174ee
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-10 21:32:31 -0400

    Displaying submenu length dynamically

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,22 +1,46 @@
-import { useRef, useEffect, useContext } from 'react';
-import { useGlobalContext, useGlobalcontext } from '../helpers/context';
+import { useRef, useEffect, useState } from 'react';
+import { useGlobalContext } from '../helpers/context';
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+  const [columns, setColumns] = useState('col-2');
   const container = useRef(null);
 
   useEffect(() => {
+    setColumns('col-2');
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+    if (links.length > 3) {
+      setColumns('col-4');
+    }
+  }, [location, links]);
 
   return (
     <aside
       className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
       ref={container}>
-      submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
